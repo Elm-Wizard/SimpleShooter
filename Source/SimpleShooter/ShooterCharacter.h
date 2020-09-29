3699ab7fe502d394b460ea3478134e4c6bab9be0
Elm-Wizard <nikoladuke97@gmail.com>
2020-09-29 12:36:30 +0200

    Initial commit - 30% features added

---
+++ Source/SimpleShooter/ShooterCharacter.h
@@ -0,0 +1,47 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Character.h"
+#include "ShooterCharacter.generated.h"
+
+class AGun;
+
+UCLASS()
+class SIMPLESHOOTER_API AShooterCharacter : public ACharacter
+{
+	GENERATED_BODY()
+
+public:
+	// Sets default values for this character's properties
+	AShooterCharacter();
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+public:	
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+	// Called to bind functionality to input
+	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
+
+private:
+	// input funkcije
+	void MoveForward(float AxisValue);
+	void MoveRight(float AxisValue);
+	void LookUpRate(float AxisValue);
+	void LookRightRate(float AxisValue);
+	void Shoot();
+
+	UPROPERTY(EditAnywhere)
+	float RotationRate = 10; // Brzina rotacije
+
+	UPROPERTY(EditDefaultsOnly)
+	TSubclassOf<AGun> GunClass; // Nije puska, vec samo klasa puske, a ne instanca
+
+	UPROPERTY()
+	AGun *Gun; // Instanca puske
+};
